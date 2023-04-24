c73ab9e872c2ce231af37eb2502a34434052ac22
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-24 09:51:33 +0100

    time1.js is completed

---
+++ timer1.js
@@ -0,0 +1,18 @@
+const beep = function() {
+    process.stdout.write('\x07');
+  }
+  
+  // delay is expected in seconds
+  const scheduleBeep = function(delay) {
+    delay = Number(delay);
+    if (!isNaN(delay) && delay > 0) {
+      setTimeout(beep, delay * 1000);
+    }
+  }
+  
+  const numbers = process.argv.slice(2);
+  console.log(numbers);
+    for (let num = 0; num <= numbers.length; num++){
+    scheduleBeep(num);
+  }
+  
